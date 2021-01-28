306c4488c2b44bf6921b269246f11c34516a66fa
nguyen.tuan.dung-b <nguyen.tuan.dung-b@sun-asterisk.com>
2021-01-28 10:47:23 +0700

    fix https

--- config/database.php
+++ config/database.php
@@ -1,5 +1,11 @@
 <?php
 
+$url = parse_url(getenv('DATABASE_URL'));
+$host = $url['host'] ?? null;
+$username = $url['user'] ?? null;
+$password = $url['pass'] ?? null;
+$database = substr($url['path'], 1) ?? null;
+
 use Illuminate\Support\Str;
 
 return [
@@ -15,7 +21,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -74,6 +80,19 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
+        'pgsql_production' => [
+            'driver' => 'pgsql',
+            'host' => $host,
+            'port' => env('DB_PORT', '5432'),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
