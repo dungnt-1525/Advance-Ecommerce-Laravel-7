35130a9b831b7d18aef1123e726800de3f6cfcd0
nguyen.tuan.dung-b <nguyen.tuan.dung-b@sun-asterisk.com>
2021-02-02 15:35:37 +0700

    Update 15.35 2.2.2021

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -52,10 +52,11 @@ class Category extends Model
     }
 
     public static function countActiveCategory() {
-        $data=Category::where('status', 'active')->count();
+        $data = Category::where('status', 'active')->count();
         if ($data) {
             return $data;
         }
+
         return 0;
     }
 }
